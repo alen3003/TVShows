105f916123a64e067ad7ed966ec9eccf00b03e2e
Alen Šebalj <sebalj.alen@gmail.com>
2022-05-03 17:00:26 +0200

    Added .swiftlint.yml and project organization

--- iOS/TvShows/TvShows/Application/AppDelegate.swift
+++ iOS/TvShows/TvShows/Application/AppDelegate.swift
@@ -3,11 +3,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        true
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        return true
     }
 
 }

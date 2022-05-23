104a289b68f89e0d8c311a5736fafa2dc45cf1e4
Alen Šebalj <sebalj.alen@gmail.com>
2022-05-23 02:38:14 +0200

    Changed content into smaller @ViewBuilder vars and methods

--- iOS/TvShows/TvShows/Common/UI/Views/ShowDetails/ShowDetails.swift
+++ iOS/TvShows/TvShows/Common/UI/Views/ShowDetails/ShowDetails.swift
@@ -8,27 +8,16 @@ struct ShowDetails: View {
     var body: some View {
         GeometryReader { proxy in
             ScrollView {
-                content
-
-                if reviews.isEmpty {
-                    Text("No reviews yet.")
-                        .font(.system(size: 17, weight: .regular))
-                        .foregroundColor(.gray)
-                }
-
-                Button("Write a Review") {
-                }
-                .frame(width: proxy.size.width - 40, height: 48)
-                .background(Color(.backgroundViolet))
-                .foregroundColor(.white)
-                .cornerRadius(24)
+                header
+                reviewList
+                footer(buttonSize: CGSize(width: proxy.size.width - 40, height: 48))
             }
         }
     }
 
     @ViewBuilder
-    var content: some View {
-        LazyVStack(alignment: .leading, spacing: 20) {
+    var header: some View {
+        VStack(alignment: .leading, spacing: 20) {
             ImageView(url: show.imageUrl, backgroundImage: .showBackground)
                 .scaledToFill()
                 .frame(height: 350)
@@ -46,10 +35,35 @@ struct ShowDetails: View {
             Text("\(show.reviewCount) REVIEWS, \(show.averageRating) AVERAGE")
                 .font(.system(size: 14, weight: .regular))
                 .foregroundColor(Color.gray)
+        }
+        .padding(20)
+    }
 
+    @ViewBuilder
+    var reviewList: some View {
+        LazyVStack(alignment: .leading, spacing: 20) {
             ForEach(reviews) { review in
                 Review(review: review)
             }
+        }
+        .padding(20)
+    }
+
+    @ViewBuilder
+    func footer(buttonSize: CGSize) -> some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if reviews.isEmpty {
+                Text("No reviews yet.")
+                    .font(.system(size: 17, weight: .regular))
+                    .foregroundColor(.gray)
+            }
+
+            Button("Write a Review") {
+            }
+            .frame(width: buttonSize.width, height: buttonSize.height)
+            .background(Color(.backgroundViolet))
+            .foregroundColor(.white)
+            .cornerRadius(24)
         }
         .padding(20)
     }
